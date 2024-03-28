1cc70cfacf96d6d8a8d3c35f9ed84a2d90f737d0
aiemtieng <pawanrataiemtieng@gmail.com>
2024-03-29 03:38:08 +0700

    imgpreviewupload

--- src/component/homepage/mainpage/MainPage.jsx
+++ src/component/homepage/mainpage/MainPage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css";
 import { CiImageOn } from "react-icons/ci";
 
 function MainPage() {
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+
+  function handleFileSelect(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      setImagePreviewUrl(e.target.result);
+    }
+
+    reader.readAsDataURL(file);
+  }
+
   return (
     <div className="MainPage">
       <div className="textdescription">
@@ -13,12 +26,13 @@ function MainPage() {
       </div>
 
       <div className="mainpageinputimage">
-        <CiImageOn style={{ fontSize: "70px",color:"white",position: "fixed",top:"38%",left: "50%",transform:"translate(-50%, -50%)" }} />
+        <CiImageOn style={{ fontSize: "70px", color: "white", position: "fixed", top: "38%", left: "50%", transform: "translate(-50%, -50%)" }} />
         <input
           className="inputfileimage"
           type="file"
           id="file"
           style={{ display: "none" }}
+          onChange={handleFileSelect}
         />
         <button
           className="buttoninputfileimage"
@@ -26,6 +40,7 @@ function MainPage() {
         >
           อัพโหลดรูปภาพ
         </button>
+        {imagePreviewUrl && <img className="imgpreview" id="preview" src={imagePreviewUrl} alt="Preview" />}
       </div>
 
       <div className="imgprocess">
